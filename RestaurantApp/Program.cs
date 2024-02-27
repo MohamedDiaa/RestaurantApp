80232887cf19ba0d990f9006e9aa1cc1e3aaca78
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2024-02-27 14:51:40 +0100

    Initial Commit, added personal register class

---
+++ RestaurantApp/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+PersonalRegister service = new PersonalRegister();
+
+service.register();
+
+
+class PersonalRegister
+{
+
+    string name;
+    string loan;
+
+    public void register()
+    {
+
+        Console.WriteLine("Please, enter employee name");
+
+        name = Console.ReadLine();
+
+        Console.WriteLine("Please, enter employee salary");
+        loan = Console.ReadLine();
+
+        Console.WriteLine("name " + name + " " + "loan " + loan);
+    }
+}
